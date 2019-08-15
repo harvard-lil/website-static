e22e770c6f353446e6881c0a8d846241045d356f
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-08-15 12:50:37 -0400

    Try logic

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-grep -r '^title: ' app | while read MATCH ; do FILE=`echo $MATCH | cut -f 1 -d ':'` ; TITLE=`echo $MATCH | cut -f 2- -d ':' | sed 's/^title: //'` ; echo $TITLE | grep ':' | grep -q -v [\'\"] && echo "$FILE needs title quoted" && exit 1 ; done
-docker-compose run jekyll jekyll build
+grep -r '^title: ' app | while read MATCH ; do FILE=`echo $MATCH | cut -f 1 -d ':'` ; TITLE=`echo $MATCH | cut -f 2- -d ':' | sed 's/^title: //'` ; echo $TITLE | grep ':' | grep -q -v [\'\"] && echo "$FILE needs title quoted" && exit 1 ; done && \
+docker-compose run jekyll jekyll build && \
 java -jar node_modules/vnu-jar/build/dist/vnu.jar --skip-non-html --errors-only --filterfile tests/config/vnufilter.txt build/
