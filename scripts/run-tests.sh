64a727311fc0443ae6c11ce130e3d75f6d5dbc45
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-08-15 14:21:52 -0400

    Try another conditional

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -14,6 +14,10 @@ validate_frontmatter () {
     return 0
 }
 
-validate_frontmatter || exit 1
-docker-compose run jekyll jekyll build && \
-java -jar node_modules/vnu-jar/build/dist/vnu.jar --skip-non-html --errors-only --filterfile tests/config/vnufilter.txt build/
+if validate_frontmatter
+then
+    docker-compose run jekyll jekyll build
+    java -jar node_modules/vnu-jar/build/dist/vnu.jar --skip-non-html --errors-only --filterfile tests/config/vnufilter.txt build/
+else
+    exit 1
+fi
