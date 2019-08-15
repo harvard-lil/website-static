f96166fcd6f1a14cbeb59bc42effd95731e95650
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-08-15 14:16:12 -0400

    Exit on validation failure

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -14,6 +14,6 @@ validate_frontmatter () {
     return 0
 }
 
-validate_frontmatter && \
+validate_frontmatter || exit 1
 docker-compose run jekyll jekyll build && \
 java -jar node_modules/vnu-jar/build/dist/vnu.jar --skip-non-html --errors-only --filterfile tests/config/vnufilter.txt build/
