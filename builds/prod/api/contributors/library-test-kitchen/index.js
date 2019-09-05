8121aad6f90f7682c1a2ad2c347c9336acbf9e54
Adam Ziegler <abziegler@gmail.com>
2019-09-05 16:15:10 -0400

    h2o developer posting

---
+++ builds/prod/api/contributors/library-test-kitchen/index.js
@@ -0,0 +1,1 @@
+contributors({"contributors": []}, "image_base": "https://lil.law.harvard.edu/assets/thumbs/216x216c", "date": "2017-12-08 19:39:52 +0000"})
---
+++ builds/prod/assets/javascripts/home.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  // header + nav should appx. fill screen on home page
+  var h = $(window).height();
+  var w = $(window).width();
+  var r = 1.8 * w;
+  if (r > h){
+    $('body.home header').height(h);
+  } else {
+    $('body.home header').height(r);
+  }
+  // roundup
+  showHopper("https://lil.law.harvard.edu/roundup/api/item");
+});
+
+function showHopper(item_url) {
+  $.getJSON(item_url + '/recent?items=5&callback=?', function(data) {
+    var items = [];
+    $.each(data.items, function(key, val) {
+      items.push('<div class="post"><div class="date"> ' + val.date + '</div><div class="content"><strong class="title"><a href="' + val.link + '">' + val.title + '</a></strong><span class="logline">' + val.description + '</span></div><div class="author">' + val.creator + '</div></div>');
+    });
+    var roundup = items.join('');
+    $('#hopper').html(roundup);
+  });
+}
