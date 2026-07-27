2e70bea6ac8ece3349899fa2bd90de502aa012d4
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2026-07-27 15:06:58 -0700

    Remove unused includes, feeds

--- app/_layouts/project_contributors_feed.js
+++
@@ -1,3 +0,0 @@
----
----
-contributors({"contributors": [{% for name in page.who_contributed %}{% assign person = people[name] %}{{ person | jsonify }}{% unless forloop.last %},{% endunless %}{% endfor %}]}, "image_base": "{{ site.url }}/assets/thumbs/216x216c", "date": "{{ 'now' | date: '%Y-%m-%d %H:%M:%S' }}"})
--- app/_layouts/project_news_feed.js
+++
@@ -1,3 +0,0 @@
----
----
-{% assign project_news = news | where: "project", page.slug %}news({"news": {{ project_news | jsonify }}, "date": "{{ 'now' | date: '%Y-%m-%d %H:%M:%S' }}"})
